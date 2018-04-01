1d7943ae47969d34bfb1f4dbad490c4b8e4262fc
ZhangGuangxu <37438456+ZhangGuangxu@users.noreply.github.com>
2018-04-02 00:03:08 +0800

    Add files via upload
    print_r is a function which prints lua values in a better way. found from internet.

---
+++ print_r/print_r.lua
@@ -0,0 +1,41 @@
+
+
+local print_r = function(t, name)  
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(val).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        local n = ''
+        if type(name) == 'string' and #name > 0 then 
+            n = '(' .. name .. ') ' 
+        end
+        print(n .. tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+return print_r
---
+++ print_r/test.lua
@@ -0,0 +1,34 @@
+
+local print_r = require 'print_r'
+
+
+local myTable = {
+    firstName = "Fred",
+    lastName = "Bob",
+    phoneNumber = "(555) 555-1212",
+    age = 30,
+    favoriteSports = { "Baseball", "Hockey", "Soccer" },
+    favoriteTeams  = { "Cowboys", "Panthers", "Reds" },
+    happy = true,
+    fight = function() return 'fight' end
+}
+
+print_r(myTable, 'myTable')
+
+
+
+local a ={
+    firstName = "Fred",
+    lastName = "Bob",
+    phoneNumber = "(555) 555-1212",
+    age = 30,
+    favoriteSports = { "Baseball", "Hockey", "Soccer" },
+    favoriteTeams  = { "Cowboys", "Panthers", "Reds" },
+    happy = true,
+    fight = function() return 'fight' end
+}
+local b = {
+    bra = a
+}
+a.arb = b
+print_r(a, 'a')
